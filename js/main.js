26e4a4ede148ff965d805fce2bd03c4e59edd46b
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-05 16:22:53 -0500

    take photo and download

--- js/main.js
+++ js/main.js
@@ -6,10 +6,20 @@
 
       const canvas = document.getElementById('canvas');
       const context = canvas.getContext('2d');
-      
+      const downloadBtn = document.getElementById('snap');
+
       setInterval(() => {
         context.drawImage(this.video, 0, 0);
       }, 1000 / 30);
+
+      downloadBtn.addEventListener('click', () => {
+        const imageURL = canvas.toDataURL('image/png');
+        const link = document.getElementById('download-link');
+
+        link.href = imageURL;
+        link.download = "photo.png";
+        link.click();
+      });
     }, err => console.log(err));
   } else {
     alert("No soportado");
